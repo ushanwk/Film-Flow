086040bf13dc5d0b594bafc64537d00941e146a2
UshanKaushalya <Ushanwk22@gmail.com>
2024-03-01 14:53:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-gray-200 font-black text-[300px]'>Film Flow</h1>
+    </div>
+  );
+}
+
+export default App;
